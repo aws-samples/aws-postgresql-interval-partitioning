8a6225010e863cb8d15d93870f216e9dad9498c8
shjeevan <shjeevan@amazon.com>
2022-03-17 15:43:49 -0600

    Update exec_pg_queries_2.sql
    Corrected formatting and data types used for some of the columns.
    
    Also modified the start position for the partitioned tables to start from 2022

--- check-cron-job-status/sql/exec_pg_queries_2.sql
+++ check-cron-job-status/sql/exec_pg_queries_2.sql
@@ -11,93 +11,90 @@ CREATE TABLE data_mart.organization
 	CONSTRAINT pk_organization PRIMARY KEY (org_id)
 );
 
-CREATE TABLE data_mart.events_daily
-(
-	event_id BIGSERIAL,
-	operation CHAR(1),
-	value FLOAT(24),
-	parent_event_id BIGINT,
-	event_type VARCHAR(25),
-	org_id BIGSERIAL,
-	created_at timestamp,
-	CONSTRAINT pk_data_mart_events_daily PRIMARY KEY (event_id, created_at),
-	CONSTRAINT ck_valid_operation CHECK (operation = 'C' OR operation = 'D'),
-	CONSTRAINT fk_orga_membership_events_daily FOREIGN KEY(org_id)
-	REFERENCES data_mart.organization (org_id),
-	CONSTRAINT fk_parent_event_id_events_daily
-	FOREIGN KEY(parent_event_id, created_at)
-	REFERENCES data_mart.events_daily (event_id,created_at)
+/* In below example, created_at column is used as partition key for the table and is also included as part of the primary key, to enforce uniqueness across partitions */
+
+CREATE TABLE data_mart.events_daily(
+    event_id        INT,
+    operation       VARCHAR(1),
+    value           FLOAT(24),
+    parent_event_id BIGINT,
+    event_type      VARCHAR(25),
+    org_id          BIGSERIAL,
+    created_at      TIMESTAMPTZ,
+    CONSTRAINT pk_data_mart_events_daily PRIMARY KEY (event_id, created_at),
+    CONSTRAINT ck_valid_operation CHECK (operation = 'C' OR operation = 'D'),
+    CONSTRAINT fk_orga_membership_events_daily FOREIGN KEY(org_id)
+    REFERENCES data_mart.organization (org_id),
+    CONSTRAINT fk_parent_event_id_events_daily FOREIGN KEY(parent_event_id, created_at)
+    REFERENCES data_mart.events_daily (event_id,created_at)
 ) PARTITION BY RANGE (created_at);
-CREATE INDEX idx_org_id_events_daily ON data_mart.events_daily(org_id);
+
+CREATE INDEX idx_org_id_events_daily     ON data_mart.events_daily(org_id);
 CREATE INDEX idx_event_type_events_daily ON data_mart.events_daily(event_type);
 
-
-CREATE TABLE data_mart.events_monthly
-(
-	event_id int,
-	value real,
-	parent_event_id bigint,
-	org_id varchar(30),
-	created_at timestamp without time zone NOT NULL,
+CREATE TABLE data_mart.events_monthly(
+	event_id        INT,
+	value           FLOAT(24),
+	parent_event_id BIGINT,
+	org_id          VARCHAR(30),
+	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_monthly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
 
-CREATE TABLE data_mart.events_quarterly
-(
-	event_id int,
-	value real,
-	parent_event_id bigint,
-	org_id varchar(30),
-	created_at timestamp without time zone NOT NULL,
+CREATE TABLE data_mart.events_quarterly(
+	event_id        INT,
+	value           FLOAT(24),
+	parent_event_id BIGINT,
+	org_id          VARCHAR(30),
+	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_quarterly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
 
-CREATE TABLE data_mart.events_yearly
-(
-	event_id int,
-	value real,
-	parent_event_id bigint,
-	org_id varchar(30),
-	created_at timestamp without time zone NOT NULL,
+CREATE TABLE data_mart.events_yearly(
+	event_id        INT,
+	value           FLOAT(24),
+	parent_event_id BIGINT,
+	org_id          VARCHAR(30),
+	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_yearly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
 
-CREATE TABLE data_mart.events_range
-(
-	event_id int,
-	value real,
-	parent_event_id bigint,
-	org_id varchar(30),
-	created_at timestamp without time zone NOT NULL,
-	CONSTRAINT pk_data_mart_events_range PRIMARY KEY (event_id, created_at)
+CREATE TABLE data_mart.events_range(
+	event_id        INT,
+	value           FLOAT(24),
+	parent_event_id BIGINT,
+	org_id          VARCHAR(30),
+	created_at      TIMESTAMPTZ,
+	CONSTRAINT pk_data_mart_events_range PRIMARY KEY (event_id)
 ) PARTITION BY RANGE (event_id);
+
 
 SELECT partman.create_parent( p_parent_table => 'data_mart.events_daily',
 p_control => 'created_at',
 p_type => 'native',
 p_interval=> 'daily',
-p_start_partition := '2021-10-01 00:00:00'::text,
+p_start_partition := '2022-01-01 00:00:00'::text,
 p_premake => 35);
 
 SELECT partman.create_parent( p_parent_table => 'data_mart.events_monthly',
 p_control => 'created_at',
 p_type => 'native',
 p_interval=> 'monthly',
-p_start_partition := '2021-10-01 00:00:00'::text,
+p_start_partition := '2022-01-01 00:00:00'::text,
 p_premake => 13);
 
 SELECT partman.create_parent( p_parent_table => 'data_mart.events_quarterly',
 p_control => 'created_at',
 p_type => 'native',
 p_interval=> '3 months',
-p_start_partition := '2021-10-01 00:00:00'::text,
+p_start_partition := '2022-01-01 00:00:00'::text,
 p_premake => 5);
 
 SELECT partman.create_parent( p_parent_table => 'data_mart.events_yearly',
 p_control => 'created_at',
 p_type => 'native',
 p_interval=> 'yearly',
-p_start_partition := '2021-10-01 00:00:00'::text,
+p_start_partition := '2022-01-01 00:00:00'::text,
 p_premake => 2);
 
 SELECT partman.create_parent( p_parent_table => 'data_mart.events_range',
@@ -106,6 +103,7 @@ p_type => 'native',
 p_interval=> '10000',
 p_start_partition := '1',
 p_premake => 3);
+
 
 
 update partman.part_config
