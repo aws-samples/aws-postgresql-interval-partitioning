d1516833dfafeb8acbf0325a5bdbbfa8429a95d6
shjeevan <shjeevan@amazon.com>
2022-04-01 07:44:06 -0600

    Update exec_pg_queries_2.sql
    Changed the data types

--- check-cron-job-status/sql/exec_pg_queries_2.sql
+++ check-cron-job-status/sql/exec_pg_queries_2.sql
@@ -4,10 +4,9 @@ CREATE EXTENSION pg_partman WITH SCHEMA partman;
 
 CREATE SCHEMA data_mart;
 
-CREATE TABLE data_mart.organization 
-( 
-	org_id BIGSERIAL,
-	org_name TEXT,
+CREATE TABLE data_mart.organization( 
+	org_id 			SERIAL,
+	org_name 		TEXT,
 	CONSTRAINT pk_organization PRIMARY KEY (org_id)
 );
 
@@ -17,9 +16,9 @@ CREATE TABLE data_mart.events_daily(
     event_id        INT,
     operation       VARCHAR(1),
     value           FLOAT(24),
-    parent_event_id BIGINT,
+    parent_event_id INT,
     event_type      VARCHAR(25),
-    org_id          BIGSERIAL,
+    org_id          INT,
     created_at      TIMESTAMPTZ,
     CONSTRAINT pk_data_mart_events_daily PRIMARY KEY (event_id, created_at),
     CONSTRAINT ck_valid_operation CHECK (operation = 'C' OR operation = 'D'),
@@ -35,8 +34,8 @@ CREATE INDEX idx_event_type_events_daily ON data_mart.events_daily(event_type);
 CREATE TABLE data_mart.events_monthly(
 	event_id        INT,
 	value           FLOAT(24),
-	parent_event_id BIGINT,
-	org_id          VARCHAR(30),
+	parent_event_id INT,
+	org_id          INT,
 	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_monthly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
@@ -44,8 +43,8 @@ CREATE TABLE data_mart.events_monthly(
 CREATE TABLE data_mart.events_quarterly(
 	event_id        INT,
 	value           FLOAT(24),
-	parent_event_id BIGINT,
-	org_id          VARCHAR(30),
+	parent_event_id INT,
+	org_id          INT,
 	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_quarterly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
@@ -53,8 +52,8 @@ CREATE TABLE data_mart.events_quarterly(
 CREATE TABLE data_mart.events_yearly(
 	event_id        INT,
 	value           FLOAT(24),
-	parent_event_id BIGINT,
-	org_id          VARCHAR(30),
+	parent_event_id INT,
+	org_id          INT,
 	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_yearly PRIMARY KEY (event_id, created_at)
 ) PARTITION BY RANGE (created_at);
@@ -62,8 +61,8 @@ CREATE TABLE data_mart.events_yearly(
 CREATE TABLE data_mart.events_range(
 	event_id        INT,
 	value           FLOAT(24),
-	parent_event_id BIGINT,
-	org_id          VARCHAR(30),
+	parent_event_id INT,
+	org_id          INT,
 	created_at      TIMESTAMPTZ,
 	CONSTRAINT pk_data_mart_events_range PRIMARY KEY (event_id)
 ) PARTITION BY RANGE (event_id);
